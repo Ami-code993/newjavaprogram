c3777e5278d8145e1b62f6366e11161056cfa68c
Ami-code993 <amitanu9043@gmail.com>
2025-04-23 23:44:56 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String [] args){
         System.out.print("this is java program");
         System.out.print("output from java program");
-        
+         System.out.print("hello world");
     }
 }
